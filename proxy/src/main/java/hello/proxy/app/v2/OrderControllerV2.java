da140e8e80ff9d5318378368645347922f594cf9
junwvwv <wvwv0612@gmail.com>
2023-09-24 13:06:00 +0900

    sample project

---
+++ proxy/src/main/java/hello/proxy/app/v2/OrderControllerV2.java
@@ -0,0 +1,31 @@
+package hello.proxy.app.v2;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@RequestMapping
+@ResponseBody
+//수동 등록을 위해 @Controller 미사용 - 스프링 부트 3.0 이상 부터는 오직 @Controller 가 있어야 컨트롤러로 인식함
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+
+    public OrderControllerV2(OrderServiceV2 orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+
+    @GetMapping("/v2/no-log")
+    public String noLog() {
+        return "ok";
+    }
+
+}
---
+++ proxy/src/main/java/hello/proxy/config/AppV1Config.java
@@ -0,0 +1,26 @@
+package hello.proxy.config;
+
+import hello.proxy.app.v1.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppV1Config {
+
+    //V1 - 인터페이스와 구현 클래스 스프링 빈으로 수동 등록
+    @Bean
+    public OrderControllerV1 orderControllerV1(){
+        return new OrderControllerV1Impl(orderServiceV1());
+    }
+
+    @Bean
+    public OrderServiceV1 orderServiceV1(){
+        return new OrderServiceV1Impl(orderRepositoryV1());
+    }
+
+    @Bean
+    public OrderRepositoryV1 orderRepositoryV1(){
+        return new OrderRepositoryV1Impl();
+    }
+
+}
